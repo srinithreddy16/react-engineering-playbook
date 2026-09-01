c571180f5812fbd8ab661381238fbc1f04d5716e
Srinith Reddy <srinithreddy2002@gmail.com>
2026-09-01 14:03:54 -0400

    one-way data binding

--- my-app/src/components/databinding/DataBinding.tsx
+++ my-app/src/components/databinding/DataBinding.tsx
@@ -1,8 +1,36 @@
-export default function DataBinding() {
+import { useState } from "react";
+function databinding() {
+    let count = 0; // Not a State Variable ( Local Variable)
+    const increment = () => {
+    count = count + 1;
+    console.log(`Count: ${count}`);
+  };
+
+  let [myCount, updateMyCount] = useState(0); // State Variable
+  const incrementCount = () => {
+    updateMyCount(myCount++);
+  };
+
+  
   return (
     <>
-      <div>Let's learn DataBinding</div>
-      <div>This is databinding Component</div>
+      <h3 className="text-center">Lets learn Data Binding</h3>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>Local variable count: {count}</div>
+        <button className="btn btn-primary" onClick={increment}>
+          Increment
+        </button>
+      </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div>State variable MyCount: {myCount}</div>
+        <button className="btn btn-primary" onClick={incrementCount}>
+          Increment
+        </button>
+      </div>
     </>
   )
 }
+
+export default databinding
