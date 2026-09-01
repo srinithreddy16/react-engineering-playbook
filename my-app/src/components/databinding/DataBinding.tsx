96c5e84c8c782a701f2c39fc837a83d8b7bd7ecb
Srinith Reddy <srinithreddy2002@gmail.com>
2026-09-01 15:38:00 -0400

    two-way data binding and conditional rendering

--- my-app/src/components/databinding/DataBinding.tsx
+++ my-app/src/components/databinding/DataBinding.tsx
@@ -1,22 +1,28 @@
+/* eslint-disable react-hooks/immutability */
 import { useState } from "react";
-function databinding() {
+
+function DataBinding() {
     let count = 0; // Not a State Variable ( Local Variable)
     const increment = () => {
     count = count + 1;
     console.log(`Count: ${count}`);
   };
 
-  let [myCount, updateMyCount] = useState(0); // State Variable
+  const [myCount, updateMyCount] = useState(0); // myCount is State Variable
   const incrementCount = () => {
-    updateMyCount(myCount++);
+    updateMyCount(myCount + 1);
   };
 
+  const [name, setName] = useState('Srinith'); //name is State variable
+
+  const [num, setNum] = useState(2);  //num is  State Variable
   
   return (
     <>
       <h3 className="text-center">Lets learn Data Binding</h3>
 
       <div className="border border-3 rounded-3 p-3 m-3">
+         <div> 1 Way Binding</div>
         <div>Local variable count: {count}</div>
         <button className="btn btn-primary" onClick={increment}>
           Increment
@@ -24,13 +30,28 @@ function databinding() {
       </div>
 
       <div className="border border-3 rounded-3 p-3 m-3">
+        <div> 1 Way Binding with State Variable</div>
         <div>State variable MyCount: {myCount}</div>
         <button className="btn btn-primary" onClick={incrementCount}>
           Increment
         </button>
       </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div> 2 Way Binding</div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <div>Name is(2-Way): {name}</div>
+      </div>
+
+      <div className="border border-3 rounded-3 p-3 m-3">
+        <div> Conditional Rendering</div>
+        <div>Even or Odd::</div>
+        <input type="number" value={num} onChange={(e) => setNum(+e.target.value)}  />
+
+        {num%2 == 0 ? <div>{num} is even Number</div> : <div>{num} is odd Number</div>}
+      </div> 
     </>
   )
 }
 
-export default databinding
+export default DataBinding
