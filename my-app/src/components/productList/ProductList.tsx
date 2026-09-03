1b638e527aba9552c4c3e9a38914bf86e3ddc78f
Srinith Reddy <srinithreddy2002@gmail.com>
2026-09-03 14:57:00 -0400

    fontawesome; sweetalert; modal

--- my-app/src/components/productList/ProductList.tsx
+++ my-app/src/components/productList/ProductList.tsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import productArr from "./product_data.ts";
+import { FaStar } from "react-icons/fa";
+import Swal from "sweetalert2";
+import Modal from 'react-modal';
 
 export default function ProductList() {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+	  function openModal() {
+		setIsOpen(true);
+	  }
+	  function closeModal() {
+		setIsOpen(false);
+	  }
+
+  const openSweetAlert = ()=>{
+    Swal.fire('Good job!','You clicked the button!', 'success')
+  }
   return (
     <>
       <h3 className="text-center">Product List</h3>
+
+      <div>
+        <button onClick={openSweetAlert}>Sweet Alert</button>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+        >
+            <h2>Hello Hiiiiiiiiiii</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+        </Modal>
+      </div>
 
       <div className="container">
         <div className="row">
@@ -16,7 +46,7 @@ export default function ProductList() {
                     <p className="card-text text-truncate">{product.title}</p>
                     <p className="card-text text-truncate">{product.description}</p>
                     <p className="card-text">{product.price}</p>
-                    <p className="card-text">{product.rating.rate}</p>
+                    <p className="card-text">{product.rating.rate} <FaStar color='gold'/></p>
                     <a href="#" className="btn btn-primary">
                       details
                     </a>
