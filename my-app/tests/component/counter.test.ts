fa8379f05ce37caa1f1fb2e4962ec438ac6448c6
Srinith Reddy <srinithreddy2002@gmail.com>
2026-09-11 23:55:21 -0400

    vitest

---
+++ my-app/tests/component/counter.test.ts
@@ -0,0 +1,8 @@
+import { describe, test } from "vitest";
+
+describe("testing Counter Component", () => { // Test Suite = Group Of Test cases
+  test("testing increment function", () => { // test case
+  });
+  test("testing decrement function", () => {});
+  test("testing reset function", () => {});
+});
---
+++ my-app/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      // Directs the coverage module to look specifically inside src
+      include: ["src/**/*.{ts,tsx,js,jsx}"],
+      exclude: ["src/main.tsx", "src/vite-env.d.ts"],
+    },
+  },
+});
