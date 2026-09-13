ded8bbf2e978a4339c7f8b07ecd8e9a1da2bf34b
Srinith Reddy <srinithreddy2002@gmail.com>
2026-09-12 23:11:10 -0400

    context api

--- my-app/src/components/counter/CounterDemo.tsx
+++ my-app/src/components/counter/CounterDemo.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { userContext } from "../../utils/app_context";
+
+
 export default function CounterDemo() {
   const [count, setCount] = useState(0);
+  const userData = useContext(userContext);
+
   return (
     <div>
+      <div>Context Data is: {userData}</div>
       <div id='count' data-testid="count">Count: {count}</div>
       <button onClick={() => setCount(count - 1)} >Decrement</button>
       <button onClick={() => setCount(0)} className="mx-2">Reset</button>
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,11 +5,15 @@ import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from 'react-router-dom';
+import { userContext } from './utils/app_context.ts';
+import { loggedInUser } from './services/user_information_service.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <userContext.Provider value={loggedInUser}>
+	      <App />
+	    </userContext.Provider>
     </BrowserRouter>
   </StrictMode>,
 )
